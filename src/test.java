51cbd23c157905d9fc61aeebd865048108c39056
kunzheng2015 <kunzheng2010@gmail.com>
2018-02-16 13:58:09 -0800

    Add files via upload

---
+++ src/test.java
@@ -0,0 +1,224 @@
+package maze;
+import java.util.Random;
+
+import javafx.application.Application; 
+import javafx.scene.Group; 
+import javafx.scene.Scene; 
+import javafx.stage.Stage; 
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+
+import javafx.scene.shape.LineTo; 
+import javafx.scene.shape.MoveTo; 
+import javafx.scene.shape.Path; 
+import java.util.Stack;
+public class test extends Application { 
+   @Override 
+   public void start(Stage stage) { 
+	   
+	   //the starting point of path
+	   int startx=20;
+	   int starty=20;
+	      
+     
+      Path path1 = new Path(); 
+      Random rand = new Random();
+      MoveTo moveTo1 = new MoveTo(0, 0); 
+      path1.getElements().add(moveTo1);
+      
+      //create the wall
+	  LineTo o1 = new LineTo(320, 0);  
+	  path1.getElements().add(o1);
+   	  
+   	  LineTo o2 = new LineTo(320, 320);  
+   	  path1.getElements().add(o2);
+ 	  
+ 	  LineTo o3 = new LineTo(0, 320);
+ 	  
+ 	  path1.getElements().add(o3);
+	  
+	  LineTo o4 = new LineTo(0, 0);  
+	  path1.getElements().add(o4);
+      
+	  
+	  //create the path 
+	  Path path = new Path(); 
+	  path.setStroke(Color.LIGHTGRAY);
+	 
+	  path.setStrokeWidth(20);
+      MoveTo moveTo = new MoveTo(startx, starty); 
+      path.getElements().add(moveTo);
+      
+      //this double array will mark the map with 1
+      //if a path create it will mark as 0
+      int [][] way=new int[8][8];
+      for(int i=0;i<8;i++)
+      {
+      	for(int j=0;j<8;j++)
+      	{
+      		way[i][j]=1;
+      	}
+      }
+      //the initial path of index is 0,0
+      //the end path of index is 7,7
+      way[0][0]=0;
+      
+      Stack st = new Stack();
+      
+      st.push(0);
+      int xi=0;
+	  int yi=0;
+	  
+	  boolean running=true;
+	  while(running)
+	  {
+		  boolean check=false;
+			int mark1=0,mark2=0,mark3=0,mark4=0;
+			System.out.println("startx:"+startx+"starty:"+starty);
+	        while(check==false)
+	        {
+	        	if(mark1==1&&mark2==1&&mark3==1&&mark4==1)
+	        	{
+	        		if(st.empty())
+	        		{
+	        			check=true;
+	        		}
+	        		else
+	        		{
+	        			int newxy=(Integer)st.pop();
+		        		int newx=newxy/10;
+		        		int newy=newxy%10;
+		        		xi=newx-1;
+		        		yi=newy-1;
+		        		startx=(xi*40)+20;
+		        		starty=(yi*40)+20;
+		        		System.out.println("newx: "+newx+" newy: "+newy);
+		        		System.out.println("newxc: "+startx+" newyc: "+starty);
+		        		MoveTo moveTo2 = new MoveTo(startx,starty); 
+		        	    path.getElements().add(moveTo2);
+		        	    
+		        	    mark1=0;mark2=0;mark3=0;mark4=0;
+	        		}
+	        		
+	        	}
+	        	int  dir = rand.nextInt(4) + 1;
+	        	switch(dir)
+	        	{
+		        	case 1:
+		        	{
+		        		mark1=1;
+		        		 int ytry=yi-1;
+		        		 if(ytry<0||way[xi][ytry]==0);
+		        		 else
+		        		 {
+		        			 check=true;
+		        			 yi=yi-1;
+		        			 way[xi][yi]=0;
+		        			 LineTo line1 = new LineTo(startx, starty=starty-40);  
+		        	      	 path.getElements().add(line1);
+		        	      	 
+		        		 }
+		        			 
+		        	}
+		        	break;
+		        	case 2:
+		        	{
+		        		mark2=1;
+		        		int xtry=xi+1;
+		        		 if(xtry>7||way[xtry][yi]==0);
+		        		 else
+		        		 {
+		        			 check=true;
+		        			 xi=xi+1;
+		        			 way[xi][yi]=0;
+		        			 LineTo line1 = new LineTo(startx=startx+40,starty);  
+		        	      	 path.getElements().add(line1);
+		        		 }
+		        	}
+		        	break;
+		        	case 3:
+		        	{
+		        		mark3=1;
+		        		int ytry=yi+1;
+		        		 if(ytry>7||way[xi][ytry]==0);
+		        		 else
+		        		 {
+		        			 check=true;
+		        			 yi=yi+1;
+		        			 way[xi][yi]=0;
+		        			 LineTo line1 = new LineTo(startx,starty=starty+40);  
+		        	      	 path.getElements().add(line1);
+		        		 }
+		        	}
+		        	break;
+		        	case 4:
+		        	{
+		        		mark4=1;
+		        		int xtry=xi-1;
+		        		 if(xtry<0||way[xtry][yi]==0);
+		        		 else
+		        		 {
+		        			 check=true;
+		        			 xi=xi-1;
+		        			 way[xi][yi]=0;
+		        			 LineTo line1 = new LineTo(startx=startx-40,starty);  
+		        	      	 path.getElements().add(line1);
+		        		 }
+		        	}
+		        	break;
+	        	}
+	        	System.out.println("xist:"+startx+"yist:"+starty+" random "+dir);
+	        }
+	        System.out.println();
+	        if(check==true)
+	        {
+	        	int xy=(xi+1)*10+(yi+1);
+	        	st.push(xy);
+	        	if(xy==88)
+	        	{
+	        		running=false;
+	        	}
+	        	System.out.println("push: "+xy+" xi:"+xi+" yi"+yi);
+	        }
+	  }
+		
+	  
+	  /*
+      for(int i=0;i<300;i+=20)
+      {
+    	  
+    	  LineTo line1 = new LineTo(i, i+10);  
+    	  path.getElements().add(line1);
+    	 // MoveTo moveTo1 = new MoveTo(i-10, i-5); 
+         // path.getElements().add(moveTo1);
+      }
+      */
+      //Creating 1st line 
+      
+      
+       
+        
+         
+      //Creating a Group object  
+      Group root = new Group(); 
+      root.getChildren().add(path1);
+      root.getChildren().add(path);
+      
+      //Creating a scene object 
+      
+      Scene scene = new Scene(root, 600, 600);  
+      
+      //Setting title to the Stage 
+      stage.setTitle("Drawing an arc through a path");
+      
+      //Adding scene to the stage 
+      stage.setScene(scene);
+      
+      //Displaying the contents of the stage 
+      stage.show();         
+   } 
+   public static void main(String args[]){ 
+      launch(args); 
+   } 
+}       
