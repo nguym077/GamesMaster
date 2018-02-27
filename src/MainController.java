aeaa844398eea9cd58a1b1b7fdc790ed72a3288e
Marian Nguyen <itsmariann@gmail.com>
2018-02-27 00:37:12 -0800

    added a Cirle to gui for user's start location with controllers from arrow buttons on screen

--- src/MainController.java
+++ src/MainController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Path;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -15,9 +16,14 @@ public class MainController {
     private GridPane mazeGrid;
 
     @FXML
-    private Text moveLabel;
+    private Text mazeMoveLabel;
+
+    @FXML
+    private Circle mazeUserLocation;
 
     Maze mazeGame = new Maze();
+    int xCurrent = 0;
+    int yCurrent = 0;
 
     public void handleMazeButton(ActionEvent e) throws Exception {
         Stage mazeStage = new Stage();
@@ -29,7 +35,6 @@ public class MainController {
         Path p = mazeGame.getPath();
         Path p1 = mazeGame.getPath1();
 
-
         mazeStage.setTitle("Maze");
         Scene scene = new Scene(root);
         mazeStage.setScene(scene);
@@ -38,22 +43,50 @@ public class MainController {
 
     public void handleMazeUpButton(ActionEvent e) {
         System.out.println("UP button was pushed.");
-        moveLabel.setText("Moved UP one space.");
+
+        if (xCurrent == 0) {
+            mazeMoveLabel.setText("Illegal move. Try again!");
+        } else {
+            mazeMoveLabel.setText("Moved UP one space.");
+            xCurrent -= 1;
+            GridPane.setRowIndex(mazeUserLocation, xCurrent);
+        }
     }
 
     public void handleMazeLeftButton(ActionEvent e) {
         System.out.println("LEFT button was pushed.");
-        moveLabel.setText("Moved LEFT one space.");
+
+        if (yCurrent == 0) {
+            mazeMoveLabel.setText("Illegal move. Try again!");
+        } else {
+            mazeMoveLabel.setText("Moved LEFT one space.");
+            yCurrent -= 1;
+            GridPane.setColumnIndex(mazeUserLocation, yCurrent);
+        }
     }
 
     public void handleMazeRightButton(ActionEvent e) {
         System.out.println("RIGHT button was pushed.");
-        moveLabel.setText("Moved RIGHT one space.");
+
+        if (yCurrent == 7) {
+            mazeMoveLabel.setText("Illegal move. Try again!");
+        } else {
+            mazeMoveLabel.setText("Moved RIGHT one space.");
+            yCurrent += 1;
+            mazeUserLocation.setLayoutY(yCurrent);
+            GridPane.setColumnIndex(mazeUserLocation, yCurrent);
+        }
     }
 
     public void handleMazeDownButton(ActionEvent e) {
         System.out.println("DOWN button was pushed.");
-        moveLabel.setText("Moved DOWN one space.");
+        if (xCurrent == 7) {
+            mazeMoveLabel.setText("Illegal move. Try again!");
+        } else {
+            mazeMoveLabel.setText("Moved DOWN one space.");
+            xCurrent += 1;
+            GridPane.setRowIndex(mazeUserLocation, xCurrent);
+        }
     }
 
     public void handleConcButton(ActionEvent e) {
