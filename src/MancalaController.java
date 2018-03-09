986c63db1a27d1fdcdabf8a0508722cf6fb56cb8
HansonKhim <khimhanson@gmail.com>
2018-03-08 16:33:25 -0800

    updated mancala controllers. currently not working on mancala

--- src/MancalaController.java
+++ src/MancalaController.java
@@ -14,13 +14,19 @@ import javafx.stage.Stage;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class MancalaController implements Initializable {
+public class MancalaController implements Initializable{
 
     @FXML
     private GridPane manacalaBoard;
 
+    @FXML
+    private Text mancalaMoveLabel;
+
+    @FXML
+    private Text scoreBoard;
 
     private int[] mancalaArray;
+    private int score[] = {0, 0};
 
     @Override
     public void initialize(URL url, ResourceBundle res) {
@@ -29,70 +35,146 @@ public class MancalaController implements Initializable {
 
     }
 
+    public boolean checkIfLeftSideIsEmpty(){
+        for(int i = 0; i < 7; i++){
+            if(mancalaArray[i] > 0 ){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean checkIfRightSideIsEmpty(){
+        for(int i = 7; i < 13; i++){
+            if(mancalaArray[i] > 0 ){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
     public void handleMancalaSpace0(ActionEvent e) {
-//        if (xCurrent == 0) {
-//            mazeMoveLabel.setText("Illegal move. Try again!");
-//        } else {
-//            String possibleMoves = Integer.toString(mazeArray[yCurrent][xCurrent]);
-//            if (possibleMoves.contains(Character.toString('1'))) {
-//                mazeMoveLabel.setText("Moved UP one space.");
-//                xCurrent -= 1;
-//                GridPane.setRowIndex(userLocation, xCurrent);
-//            } else {
-//                mazeMoveLabel.setText("Illegal move. Try again!");
-//            }
-//        }
-//
-//        if (xCurrent == 7 && yCurrent == 7) {
-//            mazeMoveLabel.setText("CONGRATS! YOU WIN! :-)");
-//            upButton.setDisable(true);
-//            rightButton.setDisable(true);
-//            downButton.setDisable(true);
-//            leftButton.setDisable(true);
-//        }
+        if (mancalaArray[0] == 0) {
+            mancalaMoveLabel.setText("Illegal move. There are no pieces in there");
+            mancalaMoveLabel.setText("Illegal move, can not pick from that side");
+        }
+        if (!checkIfLeftSideIsEmpty() || !checkIfRightSideIsEmpty()) {
+            scoreBoard.setText( score[0] + " - " + score[1]);
+        }
     }
 
     public void handleManacalaSpace1(ActionEvent e) {
+        if (mancalaArray[0] == 0) {
+            mancalaMoveLabel.setText("Illegal move. Try again!");
+        }
 
+        if (!checkIfLeftSideIsEmpty() || !checkIfRightSideIsEmpty()) {
+            scoreBoard.setText( score[0] + " - " + score[1]);
+        }
     }
 
     public void handleManacalaSpace2(ActionEvent e) {
+        if (mancalaArray[0] == 0) {
+            mancalaMoveLabel.setText("Illegal move. Try again!");
+        }
 
+        if (!checkIfLeftSideIsEmpty() || !checkIfRightSideIsEmpty()) {
+            scoreBoard.setText( score[0] + " - " + score[1]);
+        }
     }
 
     public void handleManacalaSpace3(ActionEvent e) {
+        if (mancalaArray[0] == 0) {
+            mancalaMoveLabel.setText("Illegal move. Try again!");
+        }
 
+        if (!checkIfLeftSideIsEmpty() || !checkIfRightSideIsEmpty()) {
+            scoreBoard.setText( score[0] + " - " + score[1]);
+        }
     }
 
     public void handleManacalaSpace4(ActionEvent e) {
+        if (mancalaArray[0] == 0) {
+            mancalaMoveLabel.setText("Illegal move. Try again!");
+        }
 
+        if (!checkIfLeftSideIsEmpty() || !checkIfRightSideIsEmpty()) {
+            scoreBoard.setText( score[0] + " - " + score[1]);
+        }
     }
 
     public void handleManacalaSpace5(ActionEvent e) {
+        if (mancalaArray[0] == 0) {
+            mancalaMoveLabel.setText("Illegal move. Try again!");
+        }
 
+        if (!checkIfLeftSideIsEmpty() || !checkIfRightSideIsEmpty()) {
+            scoreBoard.setText( score[0] + " - " + score[1]);
+        }
     }
 
     public void handleManacalaSpace7(ActionEvent e) {
+        if (mancalaArray[0] == 0) {
+            mancalaMoveLabel.setText("Illegal move. Try again!");
+        }
 
+        if (!checkIfLeftSideIsEmpty() || !checkIfRightSideIsEmpty()) {
+            scoreBoard.setText( score[0] + " - " + score[1]);
+
+        }
     }
 
     public void handleManacalaSpace18(ActionEvent e) {
+        if (mancalaArray[0] == 0) {
+            mancalaMoveLabel.setText("Illegal move. Try again!");
+        }
 
+        if (!checkIfLeftSideIsEmpty() || !checkIfRightSideIsEmpty()) {
+            scoreBoard.setText( score[0] + " - " + score[1]);
+
+        }
     }
 
     public void handleManacalaSpace9(ActionEvent e) {
+        if (mancalaArray[0] == 0) {
+            mancalaMoveLabel.setText("Illegal move. Try again!");
+        }
 
+        if (!checkIfLeftSideIsEmpty() || !checkIfRightSideIsEmpty()) {
+            scoreBoard.setText( score[0] + " - " + score[1]);
+        }
     }
 
     public void handleManacalaSpace10(ActionEvent e) {
+        if (mancalaArray[0] == 0) {
+            mancalaMoveLabel.setText("Illegal move. Try again!");
+        }
 
+        if (!checkIfLeftSideIsEmpty() || !checkIfRightSideIsEmpty()) {
+            scoreBoard.setText( score[0] + " - " + score[1]);
+        }
     }
 
     public void handleManacalaSpace11(ActionEvent e) {
+        if (mancalaArray[0] == 0) {
+            mancalaMoveLabel.setText("Illegal move. Try again!");
+        }
 
+        if (!checkIfLeftSideIsEmpty() || !checkIfRightSideIsEmpty()) {
+            scoreBoard.setText( score[0] + " - " + score[1]);
+
+        }
     }
 
     public void handleManacalaSpace12(ActionEvent e) {
+        if (mancalaArray[0] == 0) {
+            mancalaMoveLabel.setText("Illegal move. Try again!");
+        }
 
+        if (!checkIfLeftSideIsEmpty() || !checkIfRightSideIsEmpty()) {
+            scoreBoard.setText( score[0] + " - " + score[1]);
+        }
     }
 }
